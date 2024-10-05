6e863d654bc65751d5d66bd4fecb37af79390453
Rachel Pastreich <rachpast@bu.edu>
2024-10-05 14:13:05 -0400

    landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => {
   return (
     <>
-      <h1 className="font-merriweather">This is our landing page!</h1>
+      <div className="flex items-center justify-center h-[90vh]">
+        <div>
+          <h1 className="font-merriweather text-3xl  md:text-4xl lg:text-8xl text-center mx-5 md:mx-0">
+            Game of{" "}
+          </h1>
+          <div className="flex justify-center mt-8 md:mt-10 lg:mt-12">
+            <img
+              src="src/assets/life.png" // Ensure the path is correct
+              alt="image"
+              className="md:w-4/5 lg:w-full h-auto" // Adjusted width to make the image larger
+            />
+          </div>
+          <div className="flex items-center justify-center m-auto">
+            <Link
+              to="/quiz"
+              className="bg-[#115aad] rounded-lg py-5 px-10 text-[#ffffff] font-poppins text-center text-xl md:text-2xl lg:text-3xl" // Adjusted text size
+            >
+              Take a quiz to learn about financial literacy
+            </Link>
+          </div>
+        </div>
+      </div>
+      <footer className="mt-[] w-full bg-[#115aad] h-[8rem] flex items-center justify-center"></footer>
     </>
   );
 };

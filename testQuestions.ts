1178dbf0ed0791c2921eba59d749cf5a9f586122
kim-thing <144959722+kim-thing@users.noreply.github.com>
2024-10-05 12:34:35 -0400

    1st question

---
+++ testQuestions.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+
+
+export const QuestionList = [ 
+    {
+    id: 1,
+    question:"[Name] it's the start of a new year. You want to do it all: balance work, school, and friends all without crumbling down. You finally got your first job at Dunkin! What do you plan to do with your first check?",
+    type: "multiple-choice",
+    choices: [
+        {value: "Save all of it of course! That's what I'll do with every check ", popup: "1"},
+        {value: "Spend it! I'll start saving after this check", popup: "2"},
+        {value: "Save some and spend some, I don't want to go all out but I still wokred hard for this check", popup: "3"},
+        {value: "I'll save 70% and spend 30%", popup: "4"}
+    ]
+    }, 
+
+   
+]
+
+
+

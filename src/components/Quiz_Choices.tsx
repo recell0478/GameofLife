cee2a3a15992c24ff2ce90c534c402fd6ee0a468
stargazerJerry2023 <147276585+stargazerJerry2023@users.noreply.github.com>
2024-10-05 12:49:32 -0400

    Lmao

---
+++ src/components/Quiz_Choices.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Quiz_Choices = (Choices) => {
+    return (
+        <div>
+        <h1>Quiz Choices</h1>
+        </div>
+    );
+    }
+
+
--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+import {useState} from 'react'
+
+
 const Quiz = () => {
+  const [question, setQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [choices, setChoices] = useState([]); 
+  const 
+
   return (
-    <>
-    </>
+  
+
+
+
+
   );
 };
 

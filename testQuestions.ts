30a2e155840eb2f2ffa405dff90301253c1c924d
kim-thing <144959722+kim-thing@users.noreply.github.com>
2024-10-05 15:26:40 -0400

    Update testQuestions.ts
    update 2

--- testQuestions.ts
+++ testQuestions.ts
@@ -7,11 +7,11 @@ import react from '@vitejs/plugin-react'
 export const QuestionList = [ 
     {
     id: 1,
-    question:"[Name] it's the start of a new year. You want to do it all: balance work, school, and friends all without crumbling down. You finally got your first job at Dunkin! What do you plan to do with your first check?",
+    question:"[Name} It's the start of a new year. You want to do it all: balance work, school, and friends all without crumbling down. You finally got your first job at Dunkin! What do you plan to do with your first check?",
     type: "multiple-choice",
     choices: [
-        {value: "Save all of it of course! That's what I'll do with every check ", popup: "1"},
-        {value: "Spend it! I'll start saving after this check", popup: "2"},
+        {value: "Save all of it of course! That's what I'll do with every check ", popup: "That's great! But no. This isn't sustainable life cost money"},
+        {value: "Spend it! I'll start saving after this check", popup: "That's fair, "},
         {value: "Save some and spend some, I don't want to go all out but I still wokred hard for this check", popup: "3"},
         {value: "I'll save 70% and spend 30%", popup: "4"}
     ]
